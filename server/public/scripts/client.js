2c61e48d5144051a5ce00852f2345f95431da76f
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-09 15:36:23 -0600

    cleaned up comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,14 +5,7 @@ function onStart() {
     render()
 }
 // ! Event
-// Client: clicking the = will POST the input data:
-//  {
-//     numOne: 1,
-//     numTwo: 2,
-//     operator: '+'
-//  }
 
-//   Clear button clears the inputs
 
 // Function that will POST the infoToCompute array to the server to compute
 function compute(event) {
@@ -36,7 +29,6 @@ function compute(event) {
     })
 }
 // ! State
-// //A place to store the obj that will be sent to the server
 let infoToCompute = {
     numOne: 0,
     numTwo: 0,
@@ -46,17 +38,17 @@ let infoToCompute = {
 // A function to change the operator in the infoToCompute obj
 function assignOperator(event, operator) {
     event.preventDefault()
-    
+
     switch (operator) {
         case '+':
             infoToCompute.operator = '+'
             break
         case '-':
             infoToCompute.operator = '-'
-            break   
+            break
         case '*':
             infoToCompute.operator = '*'
-            break 
+            break
         case '/':
             infoToCompute.operator = '/'
             break
@@ -66,52 +58,31 @@ function assignOperator(event, operator) {
 
 // ! Render
 // Client:
-//  //Render on start
-//  //GET: get the data structure to render to DOM
-// // Recent RESULT displayed (the last in the array that is GETted)
-// // history minus the recent result
 
 function render() {
     axios({
         method: "GET",
         url: "/calculations"
     }).then((response) => {
-        console.log('renders. current history:', response.data);
+        //console.log('renders. current history:', response.data);
         const arrayWithHistory = response.data
-        // Displaying the result
         const recentResult = document.getElementById('recentResult')
-        //recentResult.innerHTML = ''
-        //const pForResult = document.createElement('p')
-        //pForResult.classList.add('result')
-        // Putting the last item in the array into the result section
-        if (arrayWithHistory[arrayWithHistory.length - 1]){
-            // Tests don't like when I append 😤
-        // pForResult.innerText = `Result: ${arrayWithHistory[arrayWithHistory.length - 1].result}`
-        // recentResult.append(pForResult)
-        recentResult.innerHTML = `
+        if (arrayWithHistory[arrayWithHistory.length - 1]) {
+
+            recentResult.innerHTML = `
             <p class="result">Result: ${arrayWithHistory[arrayWithHistory.length - 1].result}</p>
         `
-    }
+        }
 
         // Displaying the history
         const calculationHistory = document.getElementById('resultHistory')
         calculationHistory.innerHTML = ''
-
-        // for (let i = 0; i < arrayWithHistory.length; i++) {
-        //     const pForPastCalc = document.createElement('p')
-        //     let currentObj = arrayWithHistory[i]
-        //     pForPastCalc.innerText = `
-        //     ${currentObj.numOne} ${currentObj.operator} ${currentObj.numTwo} = ${currentObj.result}
-        //     `
-        //     calculationHistory.prepend(pForPastCalc)
-        // }
-
-            for (let i = arrayWithHistory.length -1 ; i > -1; i--) {
-                let currentObj = arrayWithHistory[i]
-                calculationHistory.innerHTML += `
+        for (let i = arrayWithHistory.length - 1; i > -1; i--) {
+            let currentObj = arrayWithHistory[i]
+            calculationHistory.innerHTML += `
                 <p>${currentObj.numOne} ${currentObj.operator} ${currentObj.numTwo} = ${currentObj.result}</p>
                 `
-            }
+        }
 
     }).catch((err) => {
         console.log(err);
